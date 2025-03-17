ddbea76a1e7cc9776511395d81591e1ed5265d13
SidEnpassant <siddhesdas2020@gmail.com>
2025-03-17 22:21:22 +0530

    Update main.dart with user authentication and routing logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:storeedge/auth/login/login_screen.dart';
-import 'package:storeedge/auth/register/register_phone_verify.dart';
-import 'package:storeedge/auth/register/register_screen.dart';
+// import 'package:storeedge/auth/register/register_phone_verify.dart';
+// import 'package:storeedge/auth/register/register_screen.dart';
 import 'package:storeedge/screens/nav-bar-call-main-screen/main_screen.dart';
 import 'package:storeedge/services/models/user_model.dart';
 import 'package:storeedge/services/settings/local_storage.dart';
-import 'package:storeedge/widgets/global/bottom%20nav%20bar/bottom_nav_bar.dart';
+// import 'package:storeedge/widgets/global/bottom%20nav%20bar/bottom_nav_bar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   User? user = await getUser(); // Retrieve user from SharedPreferences
   runApp(MyApp(user: user));
 }
+
+//
 
 class MyApp extends StatelessWidget {
   final User? user;
